715ab44f622ad6695f4101c2c97feb8b4f0bd772
Marcel Erz <erz@yahoo-inc.com>
2014-11-04 12:51:36 -0800

    Add API-documentation and Coverage report

---
+++ docs/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Company",
+        "ConnectionAdapter",
+        "Department",
+        "FileStorageAdapter",
+        "KeyValueStorageAdapter",
+        "RiakConnectionAdapter",
+        "StorageAdapter"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
